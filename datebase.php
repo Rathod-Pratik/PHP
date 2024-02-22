993c716e5b2f7766295e653c91993d095e5ce2f6
Rathod-Pratik <rathodpratik1928@gmail.com>
2024-02-22 15:52:44 +0530

    login page

--- datebase.php
+++ datebase.php
@@ -4,42 +4,49 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Database</title>
+    <style>
+      .containar{
+        background-color: #dbdbf0;
+        border: 2px solid black;
+        display: block;
+        margin: auto;
+        width: 31%;
+        height: 350px;
+      }
+      #forms{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        margin: 50px 24px;
+      }      
+
+        input{
+            height: 28px;
+            width: 310px;
+        }
+        .btns input{
+            width: 80px;
+            margin: 26px 10px;
+        }
+</style>
 </head>
 <body>
-    <?php
-    //data
-    $serverName="localhost";
-    $username="root";
-     $databasename="Royal";
-    $password="";
     
-    //connecting
-    $connect=mysqli_connect($serverName,$username,$password,$databasename);
+  <div class="containar">
+      <form action="/PHP/PHP/.database.php" id="forms">
+        <h2>Login now</h2>
+        <label for="email">Email</label>
+        <input type="email" name="email" >
 
-    //checking 
-    if(!$connect){
-        die("sorry we fali to connect= " . mysqli_connect_error());
-    }
-    else{
-        echo "connection was successful";
-    }
+        <label for="pass">Password</label>
+        <input type="password" name="pass">
 
-    //create database
-    //  $database="CREATE DATABASE Royal";
-    //  mysqli_query($connect,$database);
-    
-    // if($database){
-    //     echo "database create successfully";
-    // }
-    
-    //create table
-    //  $table="CREATE TABLE `college` (`Name` VARCHAR(13) NOT NULL , `Course` VARCHAR(6) NOT NULL , `Roll No` INT NOT  NULL AUTO_INCREMENT , `12th percentage` FLOAT NOT NULL , PRIMARY KEY (`Roll No`))";
-    //  mysqli_query($connect,$table);
-     
-     
-     //add data into table
-    //  $data="INSERT  `college` (`Name`, `Course`, `Roll No`, `12th percentage`) VALUES ('VATASAL', 'BBA', '1',   '99')";
-    //   mysqli_query($connect,$data);
-    ?>
+        <div class="btns">
+        <input type="submit" value="Submit">
+        <input type="reset" value="Reset">
+</div>
+    </form>
+  </div>
 </body>
 </html>

cdecc722b21ea2ab74fbbc4ec5a945a9544df9af
Rathod-Pratik <rathodpratik1928@gmail.com>
2024-02-23 19:37:48 +0530

    add into database

--- datebase.php
+++ datebase.php
@@ -29,24 +29,57 @@
             width: 80px;
             margin: 26px 10px;
         }
-</style>
+        </style>
 </head>
 <body>
+  <?php
+  $server="localhost";
+  $username="root";
+  $database="Royal";
+  $password="";
+  
+  $connect=mysqli_connect($server,$username,$password,$database);
+  
+  if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    $Name= $_POST['Name'];
+    $Course = $_POST['Course'];
     
-  <div class="containar">
-      <form action="/PHP/PHP/.database.php" id="forms">
-        <h2>Login now</h2>
-        <label for="email">Email</label>
-        <input type="email" name="email" >
+    $data="INSERT INTO `college` (`Name`, `Course`) VALUES ('$Name', '$Course');";
+    mysqli_query($connect,$data);
+    
+  }
+  
+  //create database using php
+  // $database="CREATE DATABASE Rathod";
+  // mysqli_query($connect,$database);
+  
+  //crate table using query
+  //let data=" CREATE TABLE .`college` (`Email` VARCHAR(30) NOT NULL , `Password` VARCHAR(13) NOT NULL ) ";
+  //mysqli_query($connect,$data);
+  
+  
+  //insert data using php
+  // $insertdata="INSERT INTO `college` (`Email`, `Password`) VALUES ('Rathodrathul1928@gmail.com', 'rathul@1234')";
+  // mysqli_query($connect,$insertdata);
+  
+  //inesrt data using form
+  
+  ?>
+    <div class="containar">
+            <form action="/PHP/PHP/datebase.php" id="forms" method="post">
+              <h2>Login now</h2>
+              <label for="Name`">Name</label>
+              <input type="text" name="Name" requested>
+        
+              <label for="Course">Course</label>
+              <input type="text" name="Course" requested>
+        
+              <div class="btns">
+              <input type="submit" value="Submit">
+              <input type="reset" value="Reset">
+            </form>
+          </div>
+    
 
-        <label for="pass">Password</label>
-        <input type="password" name="pass">
-
-        <div class="btns">
-        <input type="submit" value="Submit">
-        <input type="reset" value="Reset">
-</div>
-    </form>
-  </div>
 </body>
 </html>

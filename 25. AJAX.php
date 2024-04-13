3b03e3880c35ab9e752aab8d2c502065711c40c6
Rathod-Pratik <rathodpratik1928@gmail.com>
2024-04-13 14:01:51 +0530

    AJAX

--- 25. AJAX.php
+++ 25. AJAX.php
@@ -41,13 +41,11 @@
 
       while ($row = mysqli_fetch_array($result)) {
         echo "<option value='" . $row[0] . "'>" . $row[1] . "</option>";
-      }
-      
+ } 
       ?>
     </select>
   </form>
   <div id="texthint"></div>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 </body>
-
 </html>
